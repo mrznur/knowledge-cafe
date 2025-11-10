5a9048317fb38535ffa500d977f64472c6ce084e
mrznur <mahmudunmiraz@gmail.com>
2025-11-10 17:27:21 +0600

    added

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,14 +1,57 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
+import { CiBookmark } from "react-icons/ci";
 
-const Blog = ({blog}) => {
-    console.log(blog);
+const Blog = ({ blog }) => {
+  const {
+    title,
+    cover,
+    author,
+    author_image,
+    reading_time,
+    posted_date,
+    hashtags,
+  } = blog;
   return (
-    <div>Blog</div>
-  )
-}
+    <div className="mb-20">
+      <img
+        className="w-full rounded-md mb-8"
+        src={cover}
+        alt={`Cover picture of the ${title}`}
+      />
+      <div className="flex justify-between mb-4">
+        <div className="flex gap-5">
+          <img
+            className="w-14 rounded-full"
+            src={author_image}
+            alt={`Picture of ${author}`}
+          />
+          <div>
+            <h3 className="text-2xl">{author}</h3>
+            <p className="">{posted_date}</p>
+          </div>
+        </div>
+        <div>
+          <span className="text-lg">{reading_time}</span>
+          <button className="text-xl ml-2">
+            <CiBookmark />
+          </button>
+        </div>
+      </div>
+      <h2 className="text-4xl">{title}</h2>
+      <p>
+        {hashtags.map((hash) => (
+          <span>
+            {" "}
+            <a href="">{hash}</a>{" "}
+          </span>
+        ))}
+      </p>
+    </div>
+  );
+};
 
 Blog.propTypes = {
-    blog : PropTypes.object.isRequired
-}
+  blog: PropTypes.object.isRequired,
+};
 
-export default Blog
+export default Blog;
--- src/Components/Blogs/Blogs.jsx
+++ src/Components/Blogs/Blogs.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import Blog from '../Blog/Blog';
 
 export const Blogs = () => {
     const [blogs, setBlogs] = useState([]);
@@ -13,7 +14,13 @@ export const Blogs = () => {
    
     return (
     <div className='md:w-2/3'>
-        <h1 className="text-4xl">Blogs {blogs.length}</h1>
+        <h1 className="text-4xl mb-4">Blogs {blogs.length}</h1>
+        { 
+            blogs.map( blog => <Blog 
+                key={blog.id} 
+                blog={blog}> 
+                </Blog>)
+        }
     </div>
   )
 }

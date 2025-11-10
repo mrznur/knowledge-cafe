1edc0561ed39945aaef1a0f8d6040bd23ff7cb30
mrznur <mahmudunmiraz@gmail.com>
2025-11-10 14:53:49 +0600

    added some comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
-import React, { useState } from 'react'
-import './App.css'
-import Header from './Components/Header'
-import { Blogs } from './Components/Blogs/Blogs'
+import React, { useState } from "react";
+import "./App.css";
+import Header from "./Components/Header";
+import { Blogs } from "./Components/Blogs/Blogs";
+import Bookmarks from "./Components/Bookmarks/Bookmarks";
 function App() {
-  
-    const [count, setCount] = useState(0)
-    return (
-      <>
-       <Header>
-      
-       </Header>
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <Header></Header>
 
-       <Blogs>
-
-       </Blogs>
+      <div className="md:flex">
+        <Blogs></Blogs>
+        <Bookmarks></Bookmarks>
+      </div>
     </>
-    )
+  );
 }
 
-export default App
+export default App;
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,10 @@
-
+import profile from '../../src/assets/images/profile.png'
 const Header = () => {
   return (
-    <div>
+    <header className='flex justify-between items-center p-4 mx-4'>
         <h1 className='text-4xl font-bold'>Knowledge Cafe</h1>
-    </div>
+        <img src={profile} alt="" />
+    </header>
   )
 }
 
